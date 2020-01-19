d78b522f18cb9f01aba5137a7bf1a6d08fc83f32
d-tsuji <{{ secrets.GITHUB_EMAIL }}>
2020-01-19 11:15:08 +0000

    auto rebuilding site

--- _static/bizstyle.js
+++ _static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("Go言語入門  ドキュメント");
+        $("li.nav-item-0 a").text("Go Note  ドキュメント");
     }
 });
